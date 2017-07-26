5a0dec3d867bf933a0887ad17d49029500cf7a14
Artem Luzik <tyoma.lu@gmail.com>
2017-07-26 11:02:22 +0300

    added checkbox "EnableGrid" for test GameObjects

--- CataVOX/Assets/Testing/TestVOXMesh.cs
+++ CataVOX/Assets/Testing/TestVOXMesh.cs
@@ -4,11 +4,12 @@ using ProceduralToolkit;
 public class TestVOXMesh: MonoBehaviour
 {
     public Texture2D texture = null;
+    public bool enableGrid;
     private VOX.Model model;
 
     void Awake()
     {
         model = new VOX.Model("Assets/Testing/table.vox");
-        VOX.GameObject.SetModel(this.gameObject, model, texture, true);
+        VOX.GameObject.SetModel(this.gameObject, model, texture, enableGrid);
     }
 }
